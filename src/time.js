3faeb17a3ffc09682be92f250a257dc9c070d33c
Benjamin Cisneros Barraza <cisnerosbarraza@gmail>
2023-09-24 09:10:26 -0600

    Initial code

---
+++ src/time.js
@@ -0,0 +1,12 @@
+function time(seconds) {
+    // here is where you write your code...
+
+    if (seconds >= 60) {
+        const minutes = seconds / 60
+        return minutes + "m"
+    }
+
+    return seconds + "s"
+ }
+ 
+ module.exports = time
